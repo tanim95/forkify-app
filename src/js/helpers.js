693199f9ce2e7ce5a6be1eafedb0dc0ff248f043
tanim95 <tanimrahman78@gmail.com>
2021-03-12 22:23:12 +0600

    commit-3

---
+++ src/js/helpers.js
@@ -0,0 +1,20 @@
+import { async } from 'regenerator-runtime';
+
+const TimeOut = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took to long.Time out after ${s} secounds`));
+    }, s * 1000);
+  });
+};
+
+export const getJSON = async function (url) {
+  try {
+    const res = await Promise.race([fetch(url), TimeOut(10)]);
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
---
+++ src/js/model.js
@@ -0,0 +1,29 @@
+import { async } from 'regenerator-runtime/runtime';
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}${id}`);
+
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+      quantity: recipe.quantity,
+    };
+    console.log(data);
+  } catch (err) {
+    alert(err);
+  }
+};
