f4952712908eea3a633a1c74bf6007f68c03c6d0
tanim95 <tanimrahman78@gmail.com>
2021-03-16 14:53:15 +0600

    upto pagination

--- dist/controller.303e7ed0.js
+++ dist/controller.303e7ed0.js
@@ -5351,8 +5351,8 @@ class view {
   }
 
   render(data) {
-    if (!data || Array.isArray(data) && data.length === 0) return this.renderError();
     this._data = data;
+    if (!data || Array.isArray(data) && data.length === 0) return this.renderError();
 
     this._clear();
 
@@ -5520,31 +5520,29 @@ class paginationView extends _views.default {
 
   _generateMarkup() {
     const curPage = this._data.page;
-    const numPages = Math.ceil(this._data.results.length / this._data.resultPerPage);
-    const preview = `
-    <button data-goto ="${curPage + 1}" class="btn--inline pagination__btn--next">
-    <span>Page ${curPage + 1}</span>
-    <svg class="search__icon">
-      <use href="${_icons.default}#icon-arrow-right"></use>
-    </svg>
-  </button>
-    `;
-    const next = `
-    <button data-goto ="${curPage - 1}" class="btn--inline pagination__btn--prev">
-          <svg class="search__icon">
-            <use href="${_icons.default}#icon-arrow-left"></use>
-          </svg>
-          <span>Page ${curPage - 1}</span>
-        </button>
-    `; //page 1 ,and there are other pages
+    const numPages = Math.ceil(this._data.results.length / this._data.resultPerPage); //page 1 ,and there are other pages
 
     if (curPage === 1 && numPages > 1) {
-      return preview;
+      return `
+      <button data-goto ="${curPage + 1}" class="btn--inline pagination__btn--next">
+      <span>Page ${curPage + 1}</span>
+      <svg class="search__icon">
+        <use href="${_icons.default}#icon-arrow-right"></use>
+      </svg>
+    </button>
+      `;
     } // last page
 
 
     if (curPage === numPages && numPages > 1) {
-      return next;
+      return `
+      <button data-goto ="${curPage - 1}" class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href="${_icons.default}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${curPage - 1}</span>
+          </button>
+      `;
     } // other page
 
 
